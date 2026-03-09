e44636c5ca1be17782cc2b0b6d46bfd272a9677d
lionelenrriquez9-ui <lionelenrriquez9@gmail.com>
2026-03-09 15:03:20 -0500

    Update cumplemeli.py

--- cumplemeli.py
+++ cumplemeli.py
@@ -6,17 +6,21 @@ nombre = st.text_input("Ingresa el nombre:")
 
 if st.button("Mostrar mensaje"):
     mensaje = f"""
-¡Feliz cumpleaños, {nombre}!
+¡Feliz cumpleaños {nombre}!
 
+Bueno mali perdon por decirte feliz cumple muy tarde, perdon enserio y bueno espero que estes bien en lima y
+que te vallan bien siempre en los estudios, cuando hagas tu empresa me contratas aunque sea de limpia baños jsjajs
+y si diosito quiere nos veremos o si tu deseas tmb, nd mas que decir cuidate meli y otra vez lo digo FELIZ CUMPLEAÑOSS.
 Espero que hayas tenido un día maravilloso
 lleno de alegría y momentos especiales.
 
-Perdón por no decírtelo antes,
-pero te deseo éxitos Meli siendo sincero.
-Te quiero mucho 💖
+perdon perdon perdon por no decírtelo antes,
+espero que me perdones 🥺🥺 
+Te quiero mucho 
 """
     st.success(mensaje)
     
     # Mostrar globos
     st.balloons()
+
 
