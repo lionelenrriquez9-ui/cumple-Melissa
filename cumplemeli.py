c11ad393a6a03de0bb38e0d9068498e545951c68
lionelenrriquez9-ui <lionelenrriquez9@gmail.com>
2026-03-09 11:25:21 -0500

    Update cumplemeli.py

--- cumplemeli.py
+++ cumplemeli.py
@@ -1,38 +1,18 @@
-import tkinter as tk
-from tkinter import messagebox
+import streamlit as st
 
-def mostrar_mensaje():
-    nombre = entrada.get()
-    
+st.title("Feliz Cumpleaños 🎂")
+
+nombre = st.text_input("Ingresa el nombre:")
+
+if st.button("Mostrar mensaje"):
     mensaje = f"""
-¡Feliz cumpleaños, {nombre}! 
+¡Feliz cumpleaños, {nombre}!
 
 Espero que hayas tenido un día maravilloso
 lleno de alegría y momentos especiales.
 
 Perdón por no decírtelo antes,
-pero te deseo exitos meli siendo sincero.
+pero te deseo éxitos Meli siendo sincero.
 te quiero mucho
 """
-    
-    messagebox.showinfo("Mensaje de Cumpleaños", mensaje)
-
-# Crear ventana
-ventana = tk.Tk()
-ventana.title("Feliz Cumpleaños 🎂")
-ventana.geometry("300x150")
-
-# Texto
-label = tk.Label(ventana, text="Ingresa el nombre:")
-label.pack(pady=5)
-
-# Caja de texto
-entrada = tk.Entry(ventana)
-entrada.pack(pady=5)
-
-# Botón
-boton = tk.Button(ventana, text="Mostrar mensaje", command=mostrar_mensaje)
-boton.pack(pady=10)
-
-# Ejecutar app
-ventana.mainloop()
+    st.success(mensaje)
